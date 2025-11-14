b093f6382dd1ba4445ab9c560fa74dd9f9e7491e
gilangalfariqi <123520380+gilangalfariqi@users.noreply.github.com>
2025-11-15 04:49:58 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,30 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppProvider>(
+          create: (context) => AppProvider()
+            ..getTemperature()
+            ..getHumidity()
+            ..getSoilMoisture(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'MCS BAB 5',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
