97b0d3a432afb334747e3196b5ffac63748705f9
gilangalfariqi <123520380+gilangalfariqi@users.noreply.github.com>
2025-11-22 02:32:09 +0700

    Implement database migration functionality

---
+++ database.go
@@ -0,0 +1,23 @@
+package database
+import (
+"database/sql"
+"embed"
+"fmt"
+migrate "github.com/rubenv/sql-migrate"
+)
+//go:embed sql_migrations/*.sql
+var dbMigrations embed.FS
+var dbConnection *sql.DB
+func DBMigrate(dbParam *sql.DB) {
+migrations := &migrate.EmbedFileSystemMigrationSource{
+FileSystem: dbMigrations,
+Root: "sql_migrations",
+}
+n, errs := migrate.Exec(dbParam, "postgres", migrations,
+migrate.Up)
+if errs != nil {
+panic(errs)
+}
+DbConnection = dbParam
+fmt.Println("Migrations success applied", n, migrations)
+}
