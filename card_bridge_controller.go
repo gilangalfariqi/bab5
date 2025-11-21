8cf3cb55b68e367eff20b78c89efc57287c3bebf
gilangalfariqi <123520380+gilangalfariqi@users.noreply.github.com>
2025-11-22 02:35:49 +0700

    Refactor card bridge controller methods

--- card_bridge_controller.go
+++ card_bridge_controller.go
@@ -1,50 +1,64 @@
 package controllers
+
 import (
-"mcs_bab_6/database"
-"mcs_bab_6/entities"
-"mcs_bab_6/repositories"
-"net/http"
-"github.com/gin-gonic/gin"
+    "mcs_bab_6/database"
+    "mcs_bab_6/entities"
+    "mcs_bab_6/repositories"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
 )
+
 func GetCards(c *gin.Context) {
-var result gin.H
-card, err := repositories.GetCards(database.DbConnection)
-if err != nil {
-result = gin.H{
-"result": err.Error(),
+    var result gin.H
+
+    card, err := repositories.GetCards(database.DbConnection)
+    if err != nil {
+        result = gin.H{
+            "result": err.Error(),
+        }
+    } else {
+        result = gin.H{
+            "result": card,
+        }
+    }
+
+    c.JSON(http.StatusOK, result)
 }
-} else {
-result = gin.H{
-"result": card,
-}
-}
-c.JSON(http.StatusOK, result)
-}
+
 func InsertCard(c *gin.Context) {
-var card entities.Card
-idCard := c.Param("id")
-card.ID = idCard
+    var card entities.Card
 
-err := repositories.InsertCard(database.DbConnection, card)
-if err != nil {
-  c.JSON(http.StatusInternalServerError,
+    idCard := c.Param("id")
+    card.ID = idCard
 
-gin.H{"errpr": err.Error()})
+    err := repositories.InsertCard(database.DbConnection, card)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
 
-return
+    c.JSON(http.StatusOK, card)
 }
-c.JSON(http.StatusOK, card)
-}
+
 func DeleteCard(c *gin.Context) {
-var card entities.Card
-idCard := c.Param("id")
-card.ID = idCard
-err := repositories.DeleteCard(database.DbConnection, card)
-if err != nil {
-c.JSON(http.StatusInternalServerError,
+    var card entities.Card
 
-gin.H{"error": err.Error()})
-}
-c.JSON(http.StatusOK, gin.H{"message": "Data berhasil
-dihapus", "id": idCard})
+    idCard := c.Param("id")
+    card.ID = idCard
+
+    err := repositories.DeleteCard(database.DbConnection, card)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": err.Error(),
+        })
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "message": "Data berhasil dihapus",
+        "id":       idCard,
+    })
 }
